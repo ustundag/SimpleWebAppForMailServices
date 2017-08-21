1e38ed8beb3e3f71d8430b169a125377bca476b9
anil.ustundag <ustundaganil@gmail.com>
2017-08-21 19:02:35 +0200

    compose and search added.

--- mailmodule/server.js
+++ mailmodule/server.js
@@ -4,18 +4,26 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
+// enable Cross-origin resource sharing with CORS in localhost
+app.use(cors());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
 // Specific Mail archives
 var FILE_INBOX    = "mail_archives/inbox.mbox";
 var FILE_SENT     = "mail_archives/sent.mbox";
 var FILE_DELETED  = "mail_archives/deleted.mbox";
-
-// enable Cross-origin resource sharing with CORS in localhost
-app.use(cors());
+// read/write .mbox file module imports
+var fs					= require('fs');
+var MailParser  = require('mailparser').MailParser;
+var Mbox        = require('node-mbox');
 
 app.get('/', function (req, res) {
   res.send('Welcome to Mail Server!')
 });
-
 app.get('/inbox', function (req, res) {
   getMailArchive(FILE_INBOX, res);
 });
@@ -25,15 +33,74 @@ app.get('/sent', function (req, res) {
 app.get('/deleted', function (req, res) {
   getMailArchive(FILE_DELETED, res);
 });
+app.post('/compose', function (req, res) {
+  var mail_str = req.body.firstLine + '\n' +
+                 'From: '+ req.body.From + '\n' +
+                 'To: '  + req.body.To + '\n' +
+                 'Cc: '  + req.body.Cc + '\n' +
+                 'Bcc: ' + req.body.Bcc + '\n' +
+                 'Subject: ' + req.body.Subject + '\n' +
+                 '\n' + req.body.Message + '\n';
 
+  sendMail(mail_str);
+  console.log('Mail has been sent!');
+});
+app.get('/search', function (req, res) {
+  //searchInArchive(FILE_INBOX, req.query.keywords, res);
+  res.send(req.query.keywords);
+});
 app.listen(3000, function () {
   console.log('Mail Server started listening on port 3000!')
 });
 
-// read/write .mbox file module imports
-var MailParser  = require('mailparser').MailParser;
-var fs					= require('fs');
-var Mbox        = require('node-mbox');
+var searchInArchive = function(file, keywords, response){
+  var messages = [];
+  var mail_str = '';
+	var mbox = new Mbox();
+
+	mbox.on('message', function(msg) {
+	  // parse message using MailParser
+	  var mailparser = new MailParser({ streamAttachments : true });
+	  mailparser.on('end', function(mail) {
+      mail_str = JSON.stringify(mail);
+      if(mail_str.indexOf(keywords) > -1) {
+	  	    messages.push(mail);
+      }
+      if (messages.length == messageCount) {
+	  		//console.log('Finished parsing inbox');
+        //console.log(messages);
+        response.send(JSON.stringify(messages));
+        console.log(file + ' has been sent!');
+	  	}
+	  });
+	  mailparser.write(msg);
+	  mailparser.end();
+	});
+
+	mbox.on('end', function(parsedCount) {
+		//console.log('Completed Parsing mbox File.');
+		messageCount = parsedCount;
+	});
+
+	if (fs.existsSync(file)) {
+		//console.log('file exist!');
+		var handle = fs.createReadStream(file);
+		//handle.setEncoding('ascii');
+		handle.pipe(mbox);
+	}
+	else {
+		console.log('file not found: ' + file);
+	}
+};
+
+var sendMail = function(mail){
+  var logFile = fs.createWriteStream(FILE_SENT, {
+    flags: "a",
+    encoding: "UTF-8"
+  });
+  //call the write option where you need to append new data
+  logFile.write(mail+"\n");
+}
 
 var getMailArchive = function(file, response){
   var messages = [];
--- mailmodule/web_interface/script/main.js
+++ mailmodule/web_interface/script/main.js
@@ -1,27 +1,7 @@
 'use strict';
 
-/**
- * FloatLabels
- * Version: 1.0
- * URL: http://clubdesign.github.io/floatlabels.js/
- * Description:
- * Author: Marcus Pohorely ( http://www.clubdesign.at )
- * Copyright: Copyright 2013 / 2014 http://www.clubdesign.at
- */
-/*
-//!function(a,b){function g(b,c){this.$element=a(b),this.settings=a.extend({},f,c),this.init()}var e="floatlabel",f={slideInput:!0,labelStartTop:"20px",labelEndTop:"10px",paddingOffset:"10px",transitionDuration:.3,transitionEasing:"ease-in-out",labelClass:"",typeMatches:/text|password|email|number|search|url/};g.prototype={init:function(){var a=this,c=this.settings,d=c.transitionDuration,e=c.transitionEasing,f=this.$element,g={"-webkit-transition":"all "+d+"s "+e,"-moz-transition":"all "+d+"s "+e,"-o-transition":"all "+d+"s "+e,"-ms-transition":"all "+d+"s "+e,transition:"all "+d+"s "+e};if("INPUT"===f.prop("tagName").toUpperCase()&&c.typeMatches.test(f.attr("type"))){var h=f.attr("id");h||(h=Math.floor(100*Math.random())+1,f.attr("id",h));var i=f.attr("placeholder"),j=f.data("label"),k=f.data("class");k||(k=""),i&&""!==i||(i="You forgot to add placeholder attribute!"),j&&""!==j||(j=i),this.inputPaddingTop=parseFloat(f.css("padding-top"))+parseFloat(c.paddingOffset),f.wrap('<div class="floatlabel-wrapper" style="position:relative"></div>'),f.before('<label for="'+h+'" class="label-floatlabel '+c.labelClass+" "+k+'">'+j+"</label>"),this.$label=f.prev("label"),this.$label.css({position:"absolute",top:c.labelStartTop,left:f.css("padding-left"),display:"none","-moz-opacity":"0","-khtml-opacity":"0","-webkit-opacity":"0",opacity:"0"}),c.slideInput||f.css({"padding-top":this.inputPaddingTop}),f.on("keyup blur change",function(b){a.checkValue(b)}),b.setTimeout(function(){a.$label.css(g),a.$element.css(g)},100),this.checkValue()}},checkValue:function(a){if(a){var b=a.keyCode||a.which;if(9===b)return}var c=this.$element,d=c.data("flout");""!==c.val()&&c.data("flout","1"),""===c.val()&&c.data("flout","0"),"1"===c.data("flout")&&"1"!==d&&this.showLabel(),"0"===c.data("flout")&&"0"!==d&&this.hideLabel()},showLabel:function(){var a=this;a.$label.css({display:"block"}),b.setTimeout(function(){a.$label.css({top:a.settings.labelEndTop,"-moz-opacity":"1","-khtml-opacity":"1","-webkit-opacity":"1",opacity:"1"}),a.settings.slideInput&&a.$element.css({"padding-top":a.inputPaddingTop}),a.$element.addClass("active-floatlabel")},50)},hideLabel:function(){var a=this;a.$label.css({top:a.settings.labelStartTop,"-moz-opacity":"0","-khtml-opacity":"0","-webkit-opacity":"0",opacity:"0"}),a.settings.slideInput&&a.$element.css({"padding-top":parseFloat(a.inputPaddingTop)-parseFloat(this.settings.paddingOffset)}),a.$element.removeClass("active-floatlabel"),b.setTimeout(function(){a.$label.css({display:"none"})},1e3*a.settings.transitionDuration)}},a.fn[e]=function(b){return this.each(function(){a.data(this,"plugin_"+e)||a.data(this,"plugin_"+e,new g(this,b))})}}(jQuery,window,document);
-
-$(document).ready(function(){
-    $('.form-control').floatlabel({
-        labelClass: 'float-label',
-        labelEndTop: 5
-    });
-});
-*/
-
 $(function() {
   console.log( "ready!" );
-	REST_request_inbox();
 });
 
 var REST_request_inbox = function(){
@@ -37,14 +17,73 @@ var REST_request_inbox = function(){
       console.log(error);
     }
   });
+};
 
+var REST_request_sent = function(){
+  $.ajax({
+    url: "http://127.0.0.1:3000/sent",
+    type: 'get',
+    dataType: 'text',
+    success: function (sent) {
+      console.log(sent);
+    },
+    error: function (error) {
+      console.log("[REST_request_sent] error...");
+      console.log(error);
+    }
+  });
+};
+
+var REST_request_deleted = function(){
+  $.ajax({
+    url: "http://127.0.0.1:3000/deleted",
+    type: 'get',
+    dataType: 'text',
+    success: function (deleted) {
+      console.log(deleted);
+    },
+    error: function (error) {
+      console.log("[REST_request_deleted] error...");
+      console.log(error);
+    }
+  });
+};
+
+var REST_send_mail = function(mail){
+  $.ajax({
+    url: "http://127.0.0.1:3000/compose",
+    type: 'post',
+    contentType: "application/json; charset=utf-8",
+    dataType: 'json',
+    data: mail,
+    success: function (success) {
+      console.log(success);
+    },
+    error: function (error) {
+      console.log("[REST_send_mail] error...");
+      console.log(error);
+    }
+  });
+};
+
+var REST_search_mail = function(keyword){
+  $.ajax({
+    url: "http://127.0.0.1:3000/search",
+    type: 'get',
+    data: {
+      keywords: keyword
+    },
+    success: function (success) {
+      console.log(success);
+    },
+    error: function (error) {
+      console.log("[REST_search_mail] error...");
+      console.log(error);
+    }
+  });
 };
 
 var composeMail = function(){
-	/*let john = new User('John', 'Doe', 'john.doe@gmail.com');
-	console.log(john);*/
-
-	var mail = '';
 	var to = $('#to').val();
 	// cc and bcc should be checked with respect to ',' comma character
 	var cc = $('#cc').val();
@@ -52,15 +91,17 @@ var composeMail = function(){
 	var subject = $('#subject').val();
 	var message = $('#message').val();
 
-	//date must consist exactly 24 character
-	mail = 	'From ' + 'username ' + (new Date()) + '\n' +
-			'From: '+ '<mail_address_from>' + '\n' +
-			'To: '  + to  + '\n' +
-			'Cc: '  + cc  + '\n' +
-			'Bcc: ' + bcc + '\n' +
-			'Subject: ' + subject + '\n' +
-			'\n' + message;
+  //date must consist exactly 24 character
+  var mail_JSON = {
+                    "firstLine": "From username " + (new Date()),
+                    "From": "<mail_address_from>",
+                    "To": to,
+                    "Cc": cc,
+                    "Bcc": bcc,
+                    "Subject": subject,
+                    "Message": message
+                  };
 
-	console.log(mail);
-
+  console.log(JSON.stringify(mail_JSON));
+  REST_send_mail(JSON.stringify(mail_JSON));
 };
